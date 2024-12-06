35f3cb1457bff1200e7487af0a6cee4f7a3a09d5
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-06 12:40:32 -0500

    feat(library): add widget for charaters list.

---
+++ lib/src/library/screens/detail_screen.dart
@@ -0,0 +1,167 @@
+import 'package:anime_library_app/src/library/widgets/creator_information_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:anime_library_app/src/public/animations/fade_animation.dart';
+import 'package:anime_library_app/src/public/widgets/blur_container_widget.dart';
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen(
+      {super.key,
+      required this.color,
+      required this.image,
+      required this.name});
+
+  final int color;
+  final String name;
+  final String image;
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  double heightScreen = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    heightScreen = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(widget.color),
+              Colors.black,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  padding: const EdgeInsets.only(
+                    left: 12.0,
+                  ),
+                  child: SizedBox(
+                    height: heightScreen * .6,
+                    child: Hero(
+                      tag: widget.color,
+                      child: Image.asset("assets/${widget.image}.png"),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: BlurContainerWidget(
+                    child: Container(
+                      width: 160,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white.withOpacity(.1),
+                      ),
+                      child: Text(
+                        widget.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ),
+              child: Text(
+                "${widget.name} #Character",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ),
+              child: const Text(
+                "One Piece",
+                style: TextStyle(
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CreatorInformationWidget(
+                    title: "Eiichirō Oda",
+                    subtitle: "Author",
+                  ),
+                  CreatorInformationWidget(
+                    title: "Japan",
+                    subtitle: "Country",
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            FadeAnimation(
+                intervalStart: 0.5,
+                child: GestureDetector(
+                  onTap: () => {Navigator.pop(context)},
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 15.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(widget.color),
+                    ),
+                    child: const Text(
+                      "Back to Home",
+                      style: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/library/widgets/characters_list_widget.dart
@@ -0,0 +1,105 @@
+import 'package:anime_library_app/src/library/widgets/block_character_widget.dart';
+import 'package:anime_library_app/src/library/widgets/block_cover_widget.dart';
+import 'package:flutter/material.dart';
+
+class CharactersListWidget extends StatefulWidget {
+  const CharactersListWidget({super.key});
+
+  @override
+  State<CharactersListWidget> createState() => _CharactersListWidgetState();
+}
+
+class _CharactersListWidgetState extends State<CharactersListWidget> {
+  double widthScreen = 0;
+
+  final titleStyleText = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    widthScreen = MediaQuery.of(context).size.width - 50;
+
+    return Expanded(
+      child: ListView(
+        padding: const EdgeInsets.all(25),
+        children: [
+          Text(
+            "Covers",
+            style: titleStyleText,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              BlockCoverWidget(
+                image: "p1.jpg",
+                title: "titulo",
+                subtitle: "2018",
+                widthScreen: widthScreen,
+              ),
+              SizedBox(
+                width: widthScreen * .03,
+              ),
+              BlockCoverWidget(
+                image: "p2.jpg",
+                title: "titulo",
+                subtitle: "2022",
+                widthScreen: widthScreen,
+              ),
+              SizedBox(
+                width: widthScreen * .03,
+              ),
+              BlockCoverWidget(
+                image: "p3.jpg",
+                title: "titulo",
+                subtitle: "2023",
+                widthScreen: widthScreen,
+              ),
+            ],
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromARGB(255, 62, 62, 62),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const BlockCharacterWidget(
+            name: "Brook",
+            color: 0xff4913C4,
+            image: "o1",
+          ),
+          const BlockCharacterWidget(
+            name: "Luffy",
+            color: 0xffF82A2D,
+            image: "o2",
+          ),
+          const BlockCharacterWidget(
+            name: "Portgas D. Ace",
+            color: 0xffFFCB28,
+            image: "o3",
+          ),
+          const BlockCharacterWidget(
+            name: "Boa Hancock",
+            color: 0xffFE4649,
+            image: "o4",
+          ),
+          const BlockCharacterWidget(
+            name: "name",
+            color: 0xffDF1C6A,
+            image: "o5",
+          ),
+          const BlockCharacterWidget(
+            name: "name",
+            color: 0xff21E295,
+            image: "o6",
+          ),
+        ],
+      ),
+    );
+  }
+}
